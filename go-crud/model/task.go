3760d612eac92daccd454c712e51da276970355c
Mikołaj <dev.handbook870@slmails.com>
2026-05-28 10:49:24 +0200

    go-crud(feat): create task model

--- go-crud/model/task.go
+++ go-crud/model/task.go
@@ -1,1 +1,15 @@
 package model
+
+import "time"
+
+
+type TaskCreate struct {
+	Title		string			`json:"title"`
+}
+
+type TaskPublic struct {
+	ID			int				`json:"id"`
+	Title 		string			`json:"title"`
+	Done 		bool			`json:"done"`
+	CreatedAt 	time.Time		`json:"created_at"`
+}
